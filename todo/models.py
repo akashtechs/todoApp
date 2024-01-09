697815dae3562e1ad2cdcd6d9bfc5ecf40f36fcb
Akash <akash.misexcel@gmail.com>
2024-01-09 15:36:06 +0530

    Modified Date Field

--- todo/models.py
+++ todo/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class ToDos(models.Model):
-    date = models.DateField()
+    date = models.TextField()
     dev_name = models.CharField(max_length=20)
     pro_name = models.CharField(max_length=50)
     pro_description = models.TextField()
